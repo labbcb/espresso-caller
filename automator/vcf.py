db8a3f8236f262fda1ffd6d587a178f4486f8572
Welliton Souza <well309@gmail.com>
2019-06-26 08:25:54 -0300

    Fix collect_vcf_files to support list of dirs

--- automator/vcf.py
+++ automator/vcf.py
@@ -1,23 +1,29 @@
 from automator import search_regex
 
 
-def collect_vcf_files(directory):
+def collect_vcf_files(directories):
     """
     Collect VCF and their index files
-    :param directory: directory to search
+    :param directories: list of directories to search
     :return: two lists of VCF files and VCF index files
     """
 
-    vcf_files = search_regex(directory, '\\.vcf(\\.gz)?')
-    vcf_index_files = search_regex(directory, '\\.vcf(\\.gz)?\\.tbi')
+    all_vcf_files = []
+    all_vcf_index_files = []
+    for directory in directories:
+        vcf_files = search_regex(directory, '\\.vcf(\\.gz)?')
+        vcf_index_files = search_regex(directory, '\\.vcf(\\.gz)?\\.tbi')
 
-    vcf_len = len(vcf_files)
-    index_len = len(vcf_index_files)
+        vcf_len = len(vcf_files)
+        index_len = len(vcf_index_files)
 
-    if vcf_len == 0:
-        raise Exception('VCF files not found in {}'.format(directory))
+        if vcf_len == 0:
+            raise Exception('VCF files not found in {}'.format(directory))
 
-    if vcf_len != index_len:
-        raise Exception('VCF and index files not even. Forward: {}, Reverse: {}'.format(vcf_len, index_len))
+        if vcf_len != index_len:
+            raise Exception('VCF and index files not even. Forward: {}, Reverse: {}'.format(vcf_len, index_len))
 
-    return vcf_files, vcf_index_files
+        all_vcf_files.extend(vcf_files)
+        all_vcf_index_files.extend(vcf_index_files)
+
+    return all_vcf_files, all_vcf_index_files
