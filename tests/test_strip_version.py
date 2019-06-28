cdf4c58d1ee09abaef25e206633f39fd71df5cac
Welliton Souza <well309@gmail.com>
2019-06-28 08:41:57 -0300

    Test strip version

--- tests/test_strip_version.py
+++ tests/test_strip_version.py
@@ -1,9 +1,13 @@
-from unittest import TestCase
+import unittest
 
 from wfauto.vcf import strip_version
 
 
-class TestStripVersion(TestCase):
+class TestStripVersion(unittest.TestCase):
     def test_strip_version(self):
         self.assertEqual(strip_version('sample.b37', 'b37'), 'sample')
         self.assertEqual(strip_version('sample', 'hg38'), 'sample')
+
+
+if __name__ == '__main__':
+    unittest.main()
