d372db47c8ad6f56a707f11b09ed07bbaf4613b3
Welliton Souza <well309@gmail.com>
2019-07-11 14:28:22 -0300

    Renamed package to espresso-caller

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='espresso',
+    name='espresso-caller',
     version='1.1.0',
     packages=find_packages(),
     url='https://github.com/labbcb/espresso-caller',
