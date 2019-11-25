e99c9cff9725417a00d19197664cb365387f8591
Welliton Souza <well309@gmail.com>
2019-11-25 09:21:21 -0300

    Fix version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='espresso-caller',
-    version='1.3.0',
+    version='1.2.0',
     packages=find_packages(),
     url='https://github.com/labbcb/espresso-caller',
     license='',
