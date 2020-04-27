bed58bd138c2a81938ae429922d6c9d7adbc34d5
Welliton Souza <well309@gmail.com>
2020-04-27 12:55:33 -0300

    Fix collect_vcf_files

--- espresso/vcf.py
+++ espresso/vcf.py
@@ -5,7 +5,7 @@ from .util import search_regex, extract_sample_name
 
 def collect_vcf_files(directory, prefix='', vcf_name_regex='(?P<sample>.+?)(\\.\\w+?)?\\.g\\.vcf(\\.gz)?$'):
     """
-    Collect sample name and VCF absolute file path
+    Collect sample name and absolute path to VCF and its index file
     :param directory: list of directories to search
     :param prefix: prepend to sample name
     :param vcf_name_regex: regular expression to extract sample name from file name
@@ -13,19 +13,19 @@ def collect_vcf_files(directory, prefix='', vcf_name_regex='(?P<sample>.+?)(\\.\
     """
 
     vcf_files = search_regex(directory, '\\.g\\.vcf(\\.gz)?$')
-    # vcf_index_files = search_regex(directory, '\\.g\\.vcf(\\.gz)?\\.tbi$')
+    vcf_index_files = search_regex(directory, '\\.g\\.vcf(\\.gz)?\\.tbi$')
 
     vcf_len = len(vcf_files)
-    # index_len = len(vcf_index_files)
+    index_len = len(vcf_index_files)
 
     if vcf_len == 0:
         raise Exception('VCF files not found in {}'.format(directory))
 
-    # if vcf_len != index_len:
-    #     raise Exception('VCF and index files not even. VCF: {}, Index: {}'.format(vcf_len, index_len))
+    if vcf_len != index_len:
+        raise Exception('VCF and index files not even. VCF: {}, Index: {}'.format(vcf_len, index_len))
 
     vcf_files.sort()
-    # vcf_index_files.sort()
+    vcf_index_files.sort()
     sample_names = [prefix + extract_sample_name(f, vcf_name_regex) for f in vcf_files]
 
-    return sample_names, [abspath(vcf) for vcf in vcf_files]  # [abspath(index) for index in vcf_index_files]
+    return sample_names, [abspath(vcf) for vcf in vcf_files], [abspath(index) for index in vcf_index_files]
